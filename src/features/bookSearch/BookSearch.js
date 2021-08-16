2b366430a03d6319729ea3414b360083ce8ccadb
IGOR <kondrahov.2000@mail.ru>
2021-08-16 10:47:30 +0300

    Split BookSearch to components

--- src/features/bookSearch/BookSearch.js
+++ src/features/bookSearch/BookSearch.js
@@ -17,50 +17,62 @@ import './BookSearch.css';
 import { BookSearchInput } from './BookSearchInput';
 import { Book, ExtendedBook } from './Book';
 
-export function BookSearch() {
+function BooksList(props)
+{
+    return <div id="books-area">
+                { props.books.map((book, index) => (
+                <div className="book" key={index} onClick={() => props.onBookClick(index)}>
+                    <Book book={book}/>
+                </div>)) }
+            </div>
+}
+
+function FoundBooks()
+{
     const dispatch = useDispatch();
-    const resultsCount = useSelector(selectResultsCount);
     const books = useSelector(selectBooks);
-    const status = useSelector(selectStatus);
-    const startIndex = useSelector(selectStartIndex);
-    const chosenBook = useSelector(selectChosenBook);
     let searchParams = {
         query: useSelector(selectInputValue),
         subject: useSelector(selectSubject),
         order: useSelector(selectOrder),
-        startIndex: startIndex,
+        startIndex: useSelector(selectStartIndex),
     }
+    const resultsCount = useSelector(selectResultsCount);
 
-    let booksAreaContent = null;
-    if(chosenBook >= 0)
-    {
-        // if we have chosen any book show extended version of this book
-        booksAreaContent = (<div id="extended-book">
-                                <button className="back-to-search-results-btn" onClick={() => dispatch(setChosenBook(-1))}>
-                                    &lt;-back
-                                </button>
-                                <ExtendedBook book={books[chosenBook]}/>
-                            </div>);
-    } else {
-        // if no book is chosen show all books
-        booksAreaContent = <div>
-                                <div id="books-area">
-                                    { books.map((book, index) => (
-                                    <div className="book" key={index} onClick={() => dispatch(setChosenBook(index))}>
-                                        <Book book={book}/>
-                                    </div>)) }
-                                </div>
-                                { (books.length > 0 && books.length !== resultsCount) ? 
-                                <button className="load-more" onClick={() => {
-                                    dispatch(fetchMoreBooks(searchParams));
-                                    dispatch(setStartIndex(startIndex + books.length))
-                                }}>Load more</button> : null }
-                            </div>
-    }
+    return (<div>
+        <div>Found <span data-testid="results-num">{resultsCount}</span> results</div>
+        <BooksList books={books} onBookClick={(index) => dispatch(setChosenBook(index))} />
+        { (books.length > 0 && books.length !== resultsCount) ?
+            <button className="load-more" onClick={() => {
+                dispatch(fetchMoreBooks(searchParams));
+                dispatch(setStartIndex(searchParams.startIndex + books.length))
+            }}>Load more</button> : null }
+    </div>);
+}
+
+function ChosenBook(props)
+{
+    const dispatch = useDispatch();
+
+    return (<div id="extended-book">
+                <button className="back-to-search-results-btn"
+                    onClick={() => dispatch(setChosenBook(-1))}>
+                    &lt;-back
+                </button>
+                <ExtendedBook book={props.book}/>
+            </div>);
+}
+
+export function BookSearch() {
+    const books = useSelector(selectBooks);
+    const status = useSelector(selectStatus);
+    const chosenBook = useSelector(selectChosenBook);
 
     return (<div>
         <BookSearchInput/>
-        {chosenBook < 0 ? <div>Found <span data-testid="results-num">{resultsCount}</span> results</div> : null}
-        {status === 'loading' ? <div data-testid="loading-text">Loading...</div> : booksAreaContent}
+        {chosenBook < 0 ? 
+            <FoundBooks /> :
+            <ChosenBook book={books[chosenBook]} />}
+        {status === 'loading' ? <div data-testid="loading-text">Loading...</div> : null}
     </div>);
 }
